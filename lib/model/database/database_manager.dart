dba133bb33253c4cc8614ecbf778fb3f080d678e
RYgithub1 <ryo.rec.otani@gmail.com>
2021-01-29 16:29:05 +0900

    GoogleSignIn, Token/Credential, ConvertFirebaseToDartOfUser

--- lib/model/database/database_manager.dart
+++ lib/model/database/database_manager.dart
@@ -1,8 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+
 
 
 
 /// [V-> VM -> M(R) -> M(DatabaseManager): FireStore(database)アクセス用]
 class DatabaseManager {
+
+  /// [cloid_firestore取得用インスタンスの作成]
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+
+  /// [FireStoreから、検索条件を指定して、documentを取得する]
+  Future<bool> searchUserInDb(auth.User firebaseUser) async {
+    final query = await _db.collection("users")
+        .where(
+          "userId",   /// [DartDataClass->user.dart/property]
+          isEqualTo: firebaseUser.uid,
+        )
+        .get();
+    /// [取得結果documentにデータがある＝長さがある]
+    /// [データ存在するか否かチェックして,repositoryにreturn]
+    if(query.docs.length >= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
 
 
 }
--- lib/model/repository/user_repository.dart
+++ lib/model/repository/user_repository.dart
@@ -1,5 +1,7 @@
 /// import 'package:firebase_auth/firebase_auth.dart';   [Destructive chanegs]
 import 'package:firebase_auth/firebase_auth.dart' as auth;   /// [FirebaseAuthのものは頭にautu.xxx必須で差別化]
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:min3_minstag_ram/data_models/user.dart';
 import 'package:min3_minstag_ram/model/database/database_manager.dart';
 
 
@@ -11,9 +13,10 @@ class UserRepository {
   final DatabaseManager dbManager;
   UserRepository({this.dbManager});
 
-
   /// [(Firebase毎度のパターン)auth確認用のインスタンス生成]
   final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
 
 
   /// [remoteに取得しにいくmethodゆえRに書く（V->VM->RゆえVMから呼ぶ）]
@@ -29,8 +32,50 @@ class UserRepository {
   }
 
 
-  Future<bool> signIn() async{
 
+  Future<bool> signIn() async{
+    try {
+      GoogleSignInAccount signInAccount = await googleSignIn.signIn();
+      GoogleSignInAuthentication  signInAuthentication = await signInAccount.authentication;
+
+      /// [ggにログインして認証 -> idtoken/accesstokenで信用状credentialの作成]
+      final auth.AuthCredential credential = auth.GoogleAuthProvider.credential(
+        idToken: signInAuthentication.idToken,
+        accessToken: signInAuthentication.accessToken,
+      );
+
+      final firebaseUser = (await _auth.signInWithCredential(credential)).user;
+      /// [ifもし取得したfirebase内にuserがいなければ,早期return]
+      if(firebaseUser == null) return false;
+
+      /// [残りは,Firebase上にuserがいる場合(まずDBにそもそもいるか確認)]
+      /// [database_manager.dart見てuser有無return]
+      // await dbManager.searchUserInDb(firebaseUser);
+      final isUserExistedInDb = await dbManager.searchUserInDb(firebaseUser);
+      /// [firestore（DB）にuserいなかったら登録する]
+      if(!isUserExistedInDb) {
+        await dbManager.isertUser(_convertToUser(firebaseUser));
+        /// [_convertToUser: DDCで作成したuser.dart/propertyと、firebase/PlatformUserInfo()/propertyが異なるのｄえ変換]
+      }
+
+
+    } catch (error) {
+      print(error);
+    }
+
+  }
+
+
+
+  _convertToUser(auth.User firebaseUser) {
+    return User(
+      userId: firebaseUser.uid,
+      displayName: firebaseUser.displayName,
+      inAppUserName: firebaseUser.displayName,   /// [初期は登録時の上と同様にしとく]
+      photoUrl: firebaseUser.photoURL,
+      email: firebaseUser.email,
+      bio: "",
+    );
   }
 
 
